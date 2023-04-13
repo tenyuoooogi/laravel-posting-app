0aad1036aef031259c03fb1864c58282705b4556
tenyuoooogi <tanjoubi0103@gmail.com>
2023-04-13 22:21:43 +0900

    crudまで

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+ // やりとりするモデルを宣言する
+ use App\Models\Post;
 
-class Postcontroller extends Controller
-{
-    //  一覧ページ
-     public function index() {        
+class PostController extends Controller {
+    // 一覧ページ
+    public function index() {
         return view('posts.index');
     }
+
+    // 作成ページ
+    public function create() {
+        return view('posts.create');
+    }
+
+     // 作成機能
+     public function store(Request $request) {
+         $post = new Post();
+         $post->title = $request->input('title');
+         $post->content = $request->input('content');
+         $post->save();
+ 
+         return redirect()->route('posts.index')->with('flash_message', '投稿が完了しました。');
+     }
+
+      // 詳細ページ
+      public function show(Post $post) {
+        return view('posts.show', compact('post'));
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 投稿の一覧ページ
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+ // 投稿の作成ページ
+ Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+ 
+ // 投稿の作成機能
+ Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+// 投稿の詳細ページ
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
